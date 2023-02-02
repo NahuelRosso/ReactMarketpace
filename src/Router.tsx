96a37c27866cefc293617d32f842ce6a14d3ccfb
Nahuel Rosso <nahuelgrosso25@gmail.com>
2023-02-02 12:06:20 -0300

    Login page y manejo de errores

--- src/Router.tsx
+++ src/Router.tsx
@@ -8,9 +8,9 @@ export const AppRouter: React.FC<{}> = () => {
   return (
     <Routes>
       <Route path="/" element={<RouterLayout />}>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
       </Route>
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/nose" element={<div>Hola</div>} />
     </Routes>
   );
